ff1004b8b51aa2e5b83dcd6a21714414a50556c7
Roboticol <kaushikguha68@gmail.com>
2021-06-13 00:23:11 +0530

    added art directory

---
+++ New Unity Project/Assets/Sprites/InsertYourArtHere.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsertYourArtHere : MonoBehaviour
+{
+    /*all of your art goes into this folder, feel free to make new folders here.*/
+}
